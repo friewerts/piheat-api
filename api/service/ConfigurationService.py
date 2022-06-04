47a02c5545ac752519429ed8e6602e74287fb3c7
Frederik Riewerts <frederik.riewerts@gmail.com>
2022-06-04 17:12:35 +0200

    added custom-config handling

--- api/service/ConfigurationService.py
+++ api/service/ConfigurationService.py
@@ -1,14 +1,17 @@
 import yaml
+from dotenv import load_dotenv
 
-from os import path
+from os import path, getenv
 
 
 class ConfigurationService:
-    config_data: dict
+    default_config_data: dict
+    custom_config_data: dict
 
     def __init__(self):
-        default_config_path = path.join(path.dirname(__file__), "../config/config.yml")
-        self.config_data = ConfigurationService.get_config_data(default_config_path)
+        default_config_path = path.join(path.dirname(__file__), "../config/default_config.yml")
+        self.default_config_data = ConfigurationService.get_config_data(default_config_path)
+        self.custom_config_data = ConfigurationService.get_custom_config_data()
 
     @staticmethod
     def get_config_data(config_path):
@@ -23,5 +26,13 @@ class ConfigurationService:
             print(exc)
             return dict()
 
+    @staticmethod
+    def get_custom_config_data():
+        load_dotenv()
+        custom_config_path = getenv("CUSTOM_CONFIG_PATH")
+        if custom_config_path is None:
+            return dict()
+        return ConfigurationService.get_config_data(custom_config_path)
+
     def get_config(self, name):
-        return self.config_data.get(name)
+        return self.custom_config_data.get(name) or self.default_config_data.get(name)
